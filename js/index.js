0ae803c7708654ae03d8c7e96bebf9262b04bc5c
mehrnoush68 <mehrnoush.mohammadi@georgebrown.ca>
2020-02-14 08:54:05 -0500

    v1

--- js/index.js
+++ js/index.js
@@ -1,1 +1,19 @@
+// set the display of the .progressbar  to none
 
+// Set the display of the .progressbar tp block
+
+// store the window height
+
+// store the document height
+
+// iif documentElement height < window height
+
+//....else....
+
+// How much CAN be scrolled?
+
+// How much HAS been scrolled?
+
+//Determine the amount scrolled as a percentage of the total possible
+
+//Apply a width to the .progressbar
